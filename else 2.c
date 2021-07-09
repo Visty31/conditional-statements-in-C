7bdecac43a8fecb982c360f1c8a9ac487150f2fe
Visty31 <85346723+Visty31@users.noreply.github.com>
2021-07-09 09:46:56 +0300

    Add files via upload

---
+++ else 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//leap year test
+int main()
+{
+  int y;
+  printf("Enter the Year\n");
+  scanf("%d",&y);
+  
+  if(y%4<=0)
+  {
+    printf("%d is a leap Year",y);
+  }
+  else
+  {
+    printf("%d is not a leap Year",y);
+  }
+  return 0;
+}
